6e094e2f30fc36824f5dff073e0b0895f9fd450f
Cremporium <calebroot8@gmail.com>
2025-08-31 13:30:50 -0400

    Update Population.java
    Had a few changes, mostly spelling and changing to cammel case, along with removing some uneeded code.

--- src/maths/java/Population.java
+++ src/maths/java/Population.java
@@ -3,69 +3,61 @@ import java.util.Scanner;
 
 public class Population {
 //Variables for the entirity of the Population class 
-	public Population() {}
-	private int n;
-	private int b;
-	private int i;
-	private int d;
-	private int e;
 //method for determining overall population
-	public void oaPop() {		
+	public static void overallPopulation() {		
 		final int overallPop;		
 		try( Scanner scan = new Scanner(System.in)) {
 //print and accept input for each of the variables
 			System.out.println("Please enter the overall count for the day: ");
 			n = scan.nextInt();
 			System.out.println("Please enter the number of Births: ");
-			b = scan.nextInt();
+			births = scan.nextInt();
 			System.out.println("Please enter the number of Deaths: ");
-			d = scan.nextInt();
+			deaths = scan.nextInt();
 			System.out.println("Please enter the number of Immigrants: ");
-			i = scan.nextInt();
+			immigrants = scan.nextInt();
 			System.out.println("Please enter the number of Emmigrants: ");
-			e = scan.nextInt();
+			emmigrants = scan.nextInt();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		finally {}
 //set the overallPop var to the equation and print it
-		overallPop = n + b + i - d - e;
+		overallPop = n + births + immigrants - deaths - emmigrants;
 		System.out.println("Overall pop. count: ");
 		System.out.println(overallPop);
 			
 	}		
 //Method for determining the double time of the population
-	public void dubTime() {
-		final double dTime;
+	public static void doubleTimer() {
+		final double doubleTime;
 		final double lambda;
-		if(b + d == 0) {
+		if(births + deaths > 0) {
 			try( Scanner scan = new Scanner(System.in)) {	
 				System.out.println("Please enter the number of births: ");
-				b = scan.nextInt();
+				births = scan.nextInt();
 				System.out.println("Please enter the number of deaths: ");
-				d = scan.nextInt();
+				deaths = scan.nextInt();
 			}
 			catch(Exception e) {
 				e.printStackTrace();
 			}
-			finally {}
 		}
-		lambda = (b - d) +  1;
-		dTime = 0.693/(Math.log(lambda));
+		lambda = (births - deaths) +  1;
+		doubleTime = 0.693/(Math.log(lambda));
 		System.out.println("Double time for population: ");
-		System.out.println(dTime);
+		System.out.println(doubleTime);
 	}	
 //Method for determining the half life of the population
-	public void halfTime() {
-		final double hTime;
+	public static void halfTimer() {
+		final double halfTime;
 		final double lambda;
-		if(b + d == 0) {
+		if(births + deaths == 0) {
 			try( Scanner scan = new Scanner(System.in)) {
 				System.out.println("Please enter the number of births: ");
-				b = scan.nextInt();
+				births = scan.nextInt();
 				System.out.println("Please enter the number of deaths: ");
-				d = scan.nextInt();
+				deaths = scan.nextInt();
 			}
 			catch(Exception e) {
 			e.printStackTrace();	
@@ -73,8 +65,8 @@ public class Population {
 			finally {}
 		}
 		lambda = (b - d) + 1;
-		hTime = (Math.log(.5)) / (Math.log(lambda));
+		halfTime = (Math.log(.5)) / (Math.log(lambda));
 		System.out.println("Half life for population: ");
-		System.out.println(hTime);
+		System.out.println(halfTime);
 	}
 }
